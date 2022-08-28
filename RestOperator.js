f3b9730247fb51d66987e52607f7a48e17797b4b
lonebots <jishnusurajila@gmail.com>
2022-08-28 21:15:54 +0530

    rest operator added

---
+++ RestOperator.js
@@ -0,0 +1,13 @@
+//Rest Operator 
+//gathers/collects items
+//destructuring functions
+//placements important, carfull with the same name
+//rest when declare function, spread when invoke the function
+
+//arrays
+const fruits = ['apple','orange','banana','grape','pomegranate','pear','tomato'];
+const [first,second,...rest] = fruits;
+console.log(first,second,rest);
+
+
+//objects
