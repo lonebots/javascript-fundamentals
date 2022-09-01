3a367b7793e0674f62109e14dd866530faacfd36
lonebots <jishnusurajila@gmail.com>
2022-09-01 19:33:41 +0530

    updated optional chaining js file

--- OptChaining.js
+++ OptChaining.js
@@ -1,2 +1,16 @@
 //optional chaining
 
+const people = [
+  {
+    name: "bob",
+    location: { street: "123 street", timezone: { offset: "+8:00" } },
+  },
+  { name: "peter", location: { stree: "123 alph street" } },
+  {
+    name: "John",
+    location: { street: "123 redwood street", timezone: { offset: "_10:00" } },
+  },
+];
+
+
+console.log(people);
